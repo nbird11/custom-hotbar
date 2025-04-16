2e1e6be12e1781942d3c270454dbb6f94d6fbec1
Nathan Bird <nathanbirdka@gmail.com>
2025-04-15 22:22:25 -0600

    init commit

---
+++ src/main/java/dev/nbird/customhotbar/CustomHotbarClient.java
@@ -0,0 +1,11 @@
+package dev.nbird.customhotbar;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CustomHotbarClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/dev/nbird/customhotbar/CustomHotbarDataGenerator.java
@@ -0,0 +1,11 @@
+package dev.nbird.customhotbar;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CustomHotbarDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
